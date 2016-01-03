6b366daa32d35490ef28f43ea27ea0e5da725b04
edwyn <3dobjectifying@gmail.com>
2016-01-03 15:09:26 +0100

    added App2AppService and moved WIndows Host app to a new directory

---
+++ App2AppService/App2App WebServer/BlinkyApp/IService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlinkyWebService
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        string SendMessage(string message);
+
+        [OperationContract]
+        byte[] GetImageBytes(string message);
+    }
+}
